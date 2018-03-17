a3ab43f3563f7fa8d9ae7ce3cfb2e0263cd3ae37
TBisig <34490301+TBisig@users.noreply.github.com>
2018-03-16 17:16:01 -0700

    first commit

---
+++ TimeDisplay/apps/time_display/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import localtime, strftime
+
+
+def index(request):
+    context = {
+    "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+    }
+    return render(request, 'index.html', context)
