81b4d9f26cb8b29b4f5514875ce823990a0a1fa5
TBisig <34490301+TBisig@users.noreply.github.com>
2018-03-16 17:31:48 -0700

    first commit

---
+++ Poke/test_deployment/apps/login/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^processReg$', views.processReg, name="processReg"),
+    url(r'^processLog$', views.processLog, name='processLog'),
+    url(r'^$', views.index, name="index"),
+    url(r'^logout$', views.logout),
+]
---
+++ Poke/test_deployment/apps/login/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .models import User
+# Create your views here.
+def index(request):
+    return render(request, "login/index.html", {'status':True})
+
+def processReg(request):
+    confirm = request.POST['confirm']
+    response = User.objects.validate(request.POST)
+    if not response['status']:
+        return render(request, "login/index.html", response)
+    request.session['user_id']=response['user'].id
+    return redirect(reverse('dashboard:landing'))
+
+def processLog(request):
+	print "process log"
+	request.session['user_id']=User.objects.get(email=request.POST['email']).id
+	return redirect('/dashboard/')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
